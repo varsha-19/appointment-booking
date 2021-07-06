4a3955fd5f3e4ee09d4fe51bd4a5c194bf47fbe1
Varsha <36533785+varsha-19@users.noreply.github.com>
2021-07-06 09:53:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Header from './components/Header'
+import Showcase from './components/Showcase'
+import DoctorsList from './components/DoctorsList'
+import Footer from './components/Footer'
+import Login from './components/Login.js'
+import SignUp from './components/SignUp'
+import Error from './components/Error'
+
+function App() {
+  return (
+    <>
+    <Router>
+      <Header />
+
+      <Switch>
+      
+        <Route exact path='/'>
+          <Showcase />
+          <DoctorsList />
+        </Route>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Route path='/signup'>
+          <SignUp />
+        </Route>
+        <Route path='*'>
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+    </>
+  );
+}
+
+export default App
